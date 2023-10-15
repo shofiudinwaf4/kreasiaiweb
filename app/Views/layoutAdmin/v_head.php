4e7fc640dcb3a9251c27cd42f456d6dd417b07d3
shofiudinwaf4 <shofiudinwafa@gmail.com>
2023-10-15 11:23:41 +0700

    koreksi logo admin

--- app/Views/layoutAdmin/v_head.php
+++ app/Views/layoutAdmin/v_head.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Kreasi AI | Starter</title>
-
+    <link href="<?= base_url('uploads') ?>/<?= $perusahaan['logo_header']; ?>" rel="icon">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
